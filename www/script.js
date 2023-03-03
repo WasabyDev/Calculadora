edf7225ad75061a5c6e9732c9c5b8471a62538b4
WasabyDev <wasabygay@gmail.com>
2023-03-03 12:29:55 +0900

    estilização da calculadora

--- www/script.js
+++ www/script.js
@@ -1,7 +1,8 @@
 function calculo(){
 
     var primeirov, segundov, terceirov, resultado
-
+    var ultimor, ultimor2, ultioo3, ultimor4, ultimor5
+    
     primeirov = document.getElementById('valor1').value
     segundov = document.getElementById('valor2').value
     terceirov = document.getElementById('valor3').value
