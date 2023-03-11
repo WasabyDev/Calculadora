1618f02e5c8e9955e5aa8f69773f776e0002a539
WasabyDev <wasabygay@gmail.com>
2023-03-11 12:25:16 +0900

    regra de tres composta com historico

--- www/script.js
+++ www/script.js
@@ -1,30 +1,38 @@
-var resultado = ""
-var resultado1 = ""
-var resultado2 = ""
-var resultado3 = ""
-var resultado4 = ""
-var resultado5 = ""
-function calcularregrade3 () {
-    var valor1, valor2, valor3
+var historico1 = "";
+var historico2 = "";
+var historico3 = "";
+var historico4 = "";
+var historico5 = "";
 
-    valor1 = document.getElementById('valor1').value 
-    valor2 = document.getElementById('valor2').value
-    valor3 = document.getElementById('valor3').value
 
-    resultado5 = resultado4;
-    resultado4 = resultado3;
-    resultado3 = resultado2;
-    resultado2 = resultado1;
-    resultado1 = resultado;
-    
+function calcularconta() {
 
-    resultado = (valor2 * valor3) / valor1
+	var valor1 = parseFloat(document.getElementById("valor1").value);
+	var valor2 = parseFloat(document.getElementById("valor2").value);
+	var valor3 = parseFloat(document.getElementById("valor3").value);
+	var valor4 = parseFloat(document.getElementById("valor4").value);
+	var valor5 = parseFloat(document.getElementById("valor5").value);
 
-    document.getElementById('resultado').value = resultado 
-    document.getElementById('resultado1').textContent = resultado
-    document.getElementById('resultado2').textContent = resultado1
-    document.getElementById('resultado3').textContent = resultado2
-    document.getElementById('resultado4').textContent = resultado3
-    document.getElementById('resultado5').textContent = resultado4
-    
+	var vl6 = (valor2 * valor5);
+	var vl7 = (valor3 * valor4) * valor1;
+	var resultado = (vl7 / vl6);
+	document.getElementById("resultado").value = resultado;
+
+	    historico6 = historico5;
+    	historico5 = historico4;
+    	historico4 = historico3;
+    	historico3 = historico2;
+	    historico2 = historico1;
+    	historico1 = resultado;
+
+	document.getElementById("historico1").innerHTML = "Historico: " + historico1;
+	document.getElementById("historico2").innerHTML = "Historico2: " + historico2;
+	document.getElementById("historico3").innerHTML = "Historico3: " + historico3;
+	document.getElementById("historico4").innerHTML = "Historico4: " + historico4;
+	document.getElementById("historico5").innerHTML = "Historico5: " + historico5;
 }
+
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', () => {
+	inputField.value = '';
+});
